8ba5384f734bd5bea5c38d7fe237e76187e42fbb
Jose Cepero <ceperojose54@gmail.com>
2023-06-08 12:29:44 -0400

    created validation file and imported to app

---
+++ src/validation/pizzaSchema.js
@@ -0,0 +1,10 @@
+import * as yup from 'yup';
+
+const schema = yup.object().shape({
+    name: yup
+        .string()
+        .trim()
+        .min(2,"name must be at least 2 characters")  
+})
+
+export default schema;
