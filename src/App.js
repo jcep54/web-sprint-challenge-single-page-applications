01e18b46dba8740498c3e725c756053d1e65176d
Jose Cepero <ceperojose54@gmail.com>
2023-06-08 11:07:31 -0400

    fluffed up app and home.. imprted state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from "react";
+import React,{useState, useEffect} from "react";
 import { Routes, Route, Link} from 'react-router-dom';
 import PizzaForm from "./Components/pizzaForm";
 import Home from './Components/Home';
+
+
+
+
 const App = () => {
+  const [order, setOrder] = useState()
+
   return (
     <div>
+
       <h1>Za Za Pizza</h1>
-      
-      
-      <nav id="order-pizza">
-        <Link to='pizza'>Order Now!</Link>
 
+      <nav>
+        <Link to='/'>Home</Link>
       </nav>
-
+      
       <div>
         <Routes>
           <Route path='/' element={<Home/> }/>
           <Route path='pizza' element={<PizzaForm />}/>
         </Routes>
       </div>
+
+      <nav id="order-pizza">
+        <Link to='pizza'>Order Now!</Link>
+
+      </nav>
+
     </div>
   );
 };
